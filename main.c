c51aacad97afb98bfb7156a70588e4d075c7ed15
Jefferson Osowsky <116462366+osowsky@users.noreply.github.com>
2023-01-29 02:49:23 -0300

    Revert "Delete main.c"
    This reverts commit e79cbbbe8975231d7541e9610568b8b3d99f8d63.

---
+++ main.c
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*Consider a string containing lowercase characters from the Portuguese alphabet.
+You can perform two types of operations on this string:
+1. Concat a lowercase character from the Portuguese alphabet at the end of the string.
+2. Remove the last character from the string. If the string is empty, it will remain empty.
+
+Given an integer k and two strings s and t, determine if you can convert s to t using
+exactly k operations described above on s. If possible, the program prints 'yes',
+otherwise it prints 'no'.
+
+For example, string s = [a, b, c] and string t = [d, e, f]. The number of moves k = 6.
+To convert s to t, we first remove all characters using 3 moves. Then we concatenate
+each of the characters of t in the order. In the sixth move, you will have the expected
+string s. If more movements are available than necessary, they can be eliminated by
+performing multiple removals on an empty string. If there are fewer movements,
+it would not be possible to create the new string.
+
+Develop a C/C++ or Java language program that implements and uses the ConcatRemove (s, t, k) function.
+It should return the results 'yes' or 'no'.
+The function has the following parameters:
+s: starting string
+t: desired string
+k: an integer that represents the number of operations
+
+Input format
+The first line contains the string s, the starting string.
+The second line contains the string t, the desired string.
+The third line contains an integer k, the number of operations.
+
+Constrains
+a) 1 <= | s | <= 100
+b) 1 <= | t | <= 100
+c) 1 <= k <= 100
+d) s and t consists of lowercase letters of the Portuguese alphabet, ascii [a-z]
+
+Output format
+Print 'yes' if you can get the string t by executing exactly k operations on the
+string s, and print 'no' otherwise.
+
+Example 1
+blablablabla
+blablabcde
+8
+yes
+
+Explanation
+It took 5 operations to reduce the string "blablablabla" to "blablab". Then,
+3 concatenation operations (c, d, e) were needed to obtain the string "blablabcde".
+In this way, as it was possible to convert s to t using exactly k = 8 operations,
+the program printed "yes".
+
+Example 2
+tab
+aba
+6
+yes
+
+Explanation
+It took 4 operations to reduce the string s (remember that a removal operation on an empty string,
+keeps the string empty). Then, 3 concatenation operations were performed (a, b, a).
+Since it was possible to convert s to t using exactly k = 7 operations, the prog*/
+
+#ifndef BOOL
+	#define BOOL long
+#endif //BOOL
+
+#ifndef FALSE
+	#define FALSE ( BOOL )0L
+#endif //FALSE
+#ifndef TRUE
+	#define TRUE ( !FALSE )
+#endif //TRUE
+
+	
+// Return TRUE if there are not capital letters in the string.
+BOOL _IsLower( char* lpszText )
+{
+	BOOL bRes = TRUE;
+	
+	// Is valid?
+	if ( !lpszText )
+		return FALSE;
+	
+	// Has chars?
+	if ( !strlen( lpszText ) )
+		return TRUE;
+	
+	// Test letter.
+	if ( ( lpszText[ 0 ] >= 'a' ) && ( lpszText[ 0 ] <= 'z' ) )
+		bRes = bRes && _IsLower( &lpszText[ 1 ] );
+	else
+		return FALSE;
+	
+	return bRes;
+};
+
+// Return TRUE if you get the string t by executing exactly k operations on the s.
+BOOL ConcatRemove( char* s, char* t, int k )
+{
+	BOOL bRes = TRUE;
+	int iLen;
+	
+	// Validate pointer.
+	if ( ( !s ) || ( !t ) )
+		return FALSE;
+	
+	// Is strings ok?
+	if ( ( !_IsLower( s ) ) || ( !_IsLower( t ) ) )
+		return FALSE;
+	
+	// Begin alg.
+	if ( !s[ 0 ] ) // s is at the end.
+	{
+		if ( !t[ 0 ] ) // t is at the end, too.
+			return ( !k ? TRUE : FALSE );
+		else
+			bRes = bRes && ConcatRemove( s, &t[ 1 ], k - 1 ); // so far, so good.
+	}
+	else
+		bRes = ( s[ 0 ] == t[ 0 ] ? bRes && ConcatRemove( &s[ 1 ], &t[ 1 ], k ) /* so far, so good. Letters are the same. Keep walking.*/ :
+		bRes && ConcatRemove( &s[ strlen( s ) ], t, k - strlen( s ) ) /* s and t are not the same. Remove s and add t.*/ );
+	
+	return bRes;
+};
+
+int main( void )
+{
+	FILE* fp;
+	char s[ 1024 ];
+	char t[ 1024 ];
+	char r[ 32 ];
+	int k;
+	int cnt = 1;
+
+	// Open input file for testing.
+	fp = fopen( "input.txt", "rt" );
+	if ( !fp )
+		return -1;
+	
+	// Test file.
+	while ( !feof( fp ) )
+	{
+		// Read line.
+		if ( fscanf( fp, "%s %s %d %s", s, t, &k, r ) == EOF )
+		{
+			fclose( fp );
+			return 0;
+		}
+		
+		// Test input.
+		if ( !strcmp( "yes", ( ConcatRemove( s, t, k ) ? "yes" : "no" ) ) )
+			printf( "%03d: %s %s %d: ACK\n", cnt, s, t, k );
+		else
+			printf( "%03d: %s %s %d: NACK\n", cnt, s, t, k );
+
+		// Go to next test.
+		cnt++;
+	}
+	
+	// Close file.
+	fclose( fp );
+	
+	return 0;
+};
